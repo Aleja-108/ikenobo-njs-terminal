6bb108a0f326fe5f361f885fa4efb40129596c68
Aleja-108 <alejandraesteo@gmail.com>
2025-12-01 21:20:32 -0300

    correcciones

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ const app = express()
 const PORT = process.env.PORT || 3000;
 
 const corsConfig = {
-    origin: ['http://localhost:3000', 'https://midominio.com/'], // dominios permitidos
+    origin: ['http://localhost:3000', 'https://midominio.com/' , 'https://ikenobo-njs-terminal-production.up.railway.app'], // dominios permitidos
     methods: ['GET', 'POST', 'PUT', 'DELETE'],                  // métodos permitidos
     allowedHeaders: ['Content-Type', 'Authorization'],          // cabeceras permitidas
     exposedHeaders: ['Content-Length'],                         // cabeceras visibles al cliente
